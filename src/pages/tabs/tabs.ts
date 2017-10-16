1144b631c22c116524f7e774a44bebcdcba52d2e
hwlcode <hwlcode@163.com>
2017-10-16 17:47:22 +0800

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {IonicPage} from "ionic-angular";
+
+@IonicPage()
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+
+  tab1Root = 'HomePage';
+  tab2Root = 'ProductsPage';
+  tab3Root = 'ProfilePage';
+
+  constructor() {
+
+  }
+}
